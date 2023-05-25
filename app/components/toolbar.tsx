146bd69f70ebf6a06105e0e28f0a81279ee08b60
lkg <>
2023-05-25 18:19:42 +0800

    增加头部导航

--- app/components/toolbar.tsx
+++ app/components/toolbar.tsx
@@ -1,8 +1,10 @@
-import * as React from "react";
+"use client";
+import { useState } from "react";
 import { FaUserCircle, FaSignOutAlt, FaShoppingBag } from "react-icons/fa";
 import styles from "./toolbar.module.scss";
 import BotIcon from "../icons/bot.svg";
 export function Toolbar(props: { onClick?: (e: any) => void }) {
+  const [isAuth, setIsAuth] = useState(false);
   return (
     <div className={styles["toolbar"]}>
       <div className={styles["toolbar-inside"]}>
@@ -15,7 +17,7 @@ export function Toolbar(props: { onClick?: (e: any) => void }) {
             </div>
           </div>
           <div className={styles["toolbar-container-middle"]}>
-            <div className={styles["toolbar-search"]}>
+            {/* <div className={styles["toolbar-search"]}>
               <div className={styles["toolbar-search-container"]}>
                 <input
                   className={styles["toolbar-search-input"]}
@@ -28,58 +30,60 @@ export function Toolbar(props: { onClick?: (e: any) => void }) {
                   <span>搜索</span>
                 </button>
               </div>
-            </div>
+            </div> */}
           </div>
           <div className={styles["toolbar-container-right"]}>
             <div className={styles["toolbar-btns"]}>
-              <div
-                className={
-                  styles["toolbar-btn"] + " " + styles["toolbar-btn-login"]
-                }
-              >
-                <a className={styles["hasAvatar"]}>
-                  <img src="https://himg.bdimg.com/sys/portrait/item/49726c6b67363132339213.jpg" />
-                </a>
-                <div className={styles["toolbar-profile"]}>
-                  <div className={styles["profile-top"]}>
-                    <a className={styles["profile-avatar"]}>
-                      <img src="https://himg.bdimg.com/sys/portrait/item/49726c6b67363132339213.jpg" />
-                    </a>
-                    <p className={styles["profile-nickName"]}>--</p>
-                    <a className={styles["profile-no-vip"]}></a>
-                  </div>
-                  <div className={styles["profile-mid"]}>
-                    <a>
-                      <i className={styles["profile-fansCount"]}></i>
-                      粉丝
-                    </a>
-                    <a>关注</a>
-                    <a>获赞</a>
-                  </div>
-                  <div className={styles["profile-bottom"]}>
-                    <ul className={styles["border-bottom"]}>
-                      <li>
-                        <a>
-                          <FaUserCircle />
-                          个人中心
-                        </a>
-                      </li>
-                      <li>
-                        <a>
-                          <FaShoppingBag />
-                          我的订单
-                        </a>
-                      </li>
-                      <li className={styles["profile-logout"]}>
-                        <a>
-                          <FaSignOutAlt />
-                          退出
-                        </a>
-                      </li>
-                    </ul>
+              {isAuth && (
+                <div
+                  className={
+                    styles["toolbar-btn"] + " " + styles["toolbar-btn-login"]
+                  }
+                >
+                  <a className={styles["hasAvatar"]}>
+                    <img src="https://himg.bdimg.com/sys/portrait/item/49726c6b67363132339213.jpg" />
+                  </a>
+                  <div className={styles["toolbar-profile"]}>
+                    <div className={styles["profile-top"]}>
+                      <a className={styles["profile-avatar"]}>
+                        <img src="https://himg.bdimg.com/sys/portrait/item/49726c6b67363132339213.jpg" />
+                      </a>
+                      <p className={styles["profile-nickName"]}>--</p>
+                      <a className={styles["profile-no-vip"]}></a>
+                    </div>
+                    <div className={styles["profile-mid"]}>
+                      <a>
+                        <i className={styles["profile-fansCount"]}></i>
+                        粉丝
+                      </a>
+                      <a>关注</a>
+                      <a>获赞</a>
+                    </div>
+                    <div className={styles["profile-bottom"]}>
+                      <ul className={styles["border-bottom"]}>
+                        <li>
+                          <a>
+                            <FaUserCircle />
+                            个人中心
+                          </a>
+                        </li>
+                        <li>
+                          <a>
+                            <FaShoppingBag />
+                            我的订单
+                          </a>
+                        </li>
+                        <li className={styles["profile-logout"]}>
+                          <a>
+                            <FaSignOutAlt />
+                            退出
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
                   </div>
                 </div>
-              </div>
+              )}
             </div>
           </div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
 import { Analytics } from "@vercel/analytics/react";
 
 import { Home } from "./components/home";
-
+import { Toolbar } from "./components/toolbar";
 import { getServerSideConfig } from "./config/server";
-
 const serverConfig = getServerSideConfig();
-
 export default async function App() {
   return (
     <>
-      <Home />
+      <Toolbar />
+      <div style={{ marginTop: "99px", height: "100%" }}>
+        <Home />
+      </div>
       {serverConfig?.isVercel && <Analytics />}
     </>
   );
